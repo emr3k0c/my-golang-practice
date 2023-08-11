bd0b77942942b8112419454b4925bc88836c3f96
“[Emre <[kocemre007@gmail.com]>
2023-08-11 13:29:31 +0300

    I added more structs

--- structs/structs-intro.go
+++ structs/structs-intro.go
@@ -2,12 +2,30 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 	alex := person{firstName: "Alex", lastName: "Anderson"}
-	fmt.Println(alex)
+	// Also I can reassign this by
+	alex.firstName = "Emre"
+	alex.lastName = "Koc"
+	jim := person{
+		firstName: "jim",
+		lastName:  "Carry",
+		contact: contactInfo{
+			email:   "info@gmail.com",
+			zipCode: 7600,
+		},
+	}
+	fmt.Println(jim)
+
 }
