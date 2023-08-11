5d224887bb7c80df2450270d835e817f374ab9e4
“[Emre <[kocemre007@gmail.com]>
2023-08-11 17:47:27 +0300

    First commit after started interfaces.

--- maps/maps.go
+++ maps/maps.go
@@ -2,20 +2,20 @@ package main
 
 import "fmt"
 
-func main() {
-	var students map[string]string // Can be used to create empty map
-	fmt.Println(students)
-	clothes := make(map[string]string) // Can also be used to create empty map
-	fmt.Println(clothes)
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#4bf745",
-		"white": "#fffff",
-	}
-
-	fmt.Println(colors)
-	printMap(colors)
-}
+//func main() {
+//	var students map[string]string // Can be used to create empty map
+//	fmt.Println(students)
+//	clothes := make(map[string]string) // Can also be used to create empty map
+//	fmt.Println(clothes)
+//	colors := map[string]string{
+//		"red":   "#ff0000",
+//		"green": "#4bf745",
+//		"white": "#fffff",
+//	}
+//
+//	fmt.Println(colors)
+//	printMap(colors)
+//}
 
 func printMap(c map[string]string) {
 	for color, hex := range c {
