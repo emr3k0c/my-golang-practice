4306501d7ef05e917fb14f2b108ac6035b9d76f7
“[Emre <[kocemre007@gmail.com]>
2023-08-11 17:02:41 +0300

    Maps are done

--- maps/maps.go
+++ maps/maps.go
@@ -10,7 +10,16 @@ func main() {
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#4bf745",
+		"white": "#fffff",
 	}
 
 	fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println(color, hex)
+
+	}
 }
