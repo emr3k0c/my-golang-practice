e4eb59a850ac1bf2da1c335fa3bb883fd2d2e152
“[Emre <[kocemre007@gmail.com]>
2023-08-12 17:19:49 +0300

    I looked and Reader and Writer interface

--- interfaces/interface.go
+++ interfaces/interface.go
@@ -11,12 +11,12 @@ type englishBot struct {
 type spanishBot struct {
 }
 
-func main() {
-	eb := englishBot{}
-	sb := spanishBot{}
-	printGreeting(eb)
-	printGreeting(sb)
-}
+//	func main() {
+//		eb := englishBot{}
+//		sb := spanishBot{}
+//		printGreeting(eb)
+//		printGreeting(sb)
+//	}
 func printGreeting(b bot) {
 	fmt.Println(b.getGreeting())
 }
