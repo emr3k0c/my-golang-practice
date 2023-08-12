f130a060b37c533af2bcf1ce4024c3e06b4c3929
“[Emre <[kocemre007@gmail.com]>
2023-08-12 17:27:10 +0300

    Interface implementation is done

---
+++ interfaces/assignment.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type triangle struct {
+	height float64
+	base   float64
+}
+type square struct {
+	sideLength float64
+}
+
+func main() {
+	tri := triangle{
+		height: 15,
+		base:   17,
+	}
+	squ := square{
+		sideLength: 20,
+	}
+	printArea(tri)
+	printArea(squ)
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.height * t.base
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
