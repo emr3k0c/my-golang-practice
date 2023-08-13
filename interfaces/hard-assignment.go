a00a81d3fd1fb93e29c9670220375fb500886464
“[Emre <[kocemre007@gmail.com]>
2023-08-13 23:26:48 +0300

    I have completed my golang course

--- interfaces/hard-assignment.go
+++ interfaces/hard-assignment.go
@@ -1,15 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"os"
-)
-
-func main() {
-	file, err := os.Open("/Users/emrekoc/GolandProjects/my-golang-practice/interfaces/file.txt")
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-	io.Copy(os.Stdout, file)
-}
+//func main() {
+//	file, err := os.Open("/Users/emrekoc/GolandProjects/my-golang-practice/interfaces/file.txt")
+//	if err != nil {
+//		fmt.Println("Error: ", err)
+//	}
+//	io.Copy(os.Stdout, file)
+//}
